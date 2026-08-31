0dc021af0dde8f3fd5e5632252118b1cf676742b
GauravPhysics12 <gauravphysics1212@gmail.com>
2026-08-31 21:05:23 +0530

    31st Aug

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,7 +3,7 @@ export default {
 	description: ``,
 	url: `https://GauravPhysics12.github.io`,
 	name: `Gaurav Rachh`,
-	enableThemeSwitcher: false,
-	theme: 'light', // ✅ This must be set to 'light'
+	enableThemeSwitcher: true,
+	theme: 'dark',
 	showPlug: false,
 };
